234cd3b4454059d3370cead3b479105640d70d84
Documenter.jl <documenter@juliadocs.github.io>
2022-07-29 15:38:59 +0000

    build based on e0bc987

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#KFUPMMathHW.jl","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"","category":"section"},{"location":"","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"Documentation for KFUPMMathHW.jl","category":"page"}]
+[{"location":"#KFUPMMathHW.jl","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"","category":"section"},{"location":"","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"Documentation for KFUPMMathHW.jl","category":"page"},{"location":"","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"Modules = [KFUPMMathHW]","category":"page"}]
 }
