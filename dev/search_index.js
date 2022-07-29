61a634e5c1de11547999136f05ed1fda319da479
Documenter.jl <documenter@juliadocs.github.io>
2022-07-29 07:43:45 +0000

    build based on 182c6c8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#KFUPMMathHW.jl","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"","category":"section"},{"location":"","page":"KFUPMMathHW.jl","title":"KFUPMMathHW.jl","text":"Documentation for KFUPMMathHW.jl","category":"page"}]
+}
